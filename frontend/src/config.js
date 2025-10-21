745005d9ff7a783322ba1fabcb7446319fdb5eef
Yifeng <2833879607@qq.com>
2025-10-21 21:39:52 +0800

    edit config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://localhost:5000/api'
-// const DEFAULT_AVATAR = `${process.env.WEB_BASE_URL}/default-avatar.png`;
-const DEFAULT_AVATAR = `${BASE_URL}/files/default-avatar.png`;
+const DEFAULT_AVATAR = `${process.env.REACT_APP_WEB_BASE_URL}/default-avatar.png`;
+
 export { BASE_URL, DEFAULT_AVATAR };
